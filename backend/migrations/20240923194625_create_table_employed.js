808fdd8a99d439868247bbe2431021e4ab54eccb
Brandon Gento <brandonailon19@gmail.com>
2024-09-23 15:14:00 -0600

    Migraciones de tabla empleados

---
+++ backend/migrations/20240923194625_create_table_employed.js
@@ -0,0 +1,31 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+
+const db = require('../models/db');
+
+exports.up = function(knex) {
+    return knex.schema.hasTable('employed').then(function(exists) {
+        if (!exists) {
+            return knex.schema.createTable('employed', (table) => {
+                table.increments('id').unique();
+                table.string('name').notNullable();
+                table.string('last_name').notNullable();
+                table.string('dpi').unique().notNullable();
+                table.string('number_IGGS').unique().notNullable();
+                table.string('phone_number').notNullable();
+                table.string('number_NIT').unique().notNullable();
+            });
+        }
+    });
+};
+
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};
