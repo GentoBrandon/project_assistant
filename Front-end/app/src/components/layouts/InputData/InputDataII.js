26df72c1242efcf013f05932b6ade67fc0eb0bf4
Brandon Gento <brandonailon19@gmail.com>
2024-09-29 11:54:43 -0600

    Validaciones de formulario Empleado

--- Front-end/app/src/components/layouts/InputData/InputDataII.js
+++ Front-end/app/src/components/layouts/InputData/InputDataII.js
@@ -2,10 +2,10 @@ import Form from 'react-bootstrap/Form';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 
 
-function InputDataII ({name, type, placeholder, id, Feedback}) {
+function InputDataII ({name, type, placeholder, id, Feedback, as}) {
     
     return (
-        <Form.Group className="mb-3" controlId={id}>
+        <Form.Group className="mb-3" controlId={id} as={as}>
                 <Form.Label>{name}</Form.Label>
                 <Form.Control type={type} placeholder={placeholder}  required />
                 <Form.Control.Feedback type="invalid">{Feedback}</Form.Control.Feedback>
--- Front-end/app/src/components/template/formEmployed/FormEmTemplate.js
+++ Front-end/app/src/components/template/formEmployed/FormEmTemplate.js
@@ -3,25 +3,67 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import InputDataII from '@/components/layouts/InputData/InputDataII';
 import Buttons from '@/components/layouts/InputData/Buttons';
 import styles from '../../../styles/FormStyles.module.css';
+import { useState } from 'react';
+import { Form, Row } from 'react-bootstrap';
+import Col from 'react-bootstrap/Col';
 
-function FormEmployed() {  // El nombre empieza con mayúscula
+function FormEmployed() {  
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+        }
+        setValidated(true);
+    }
     return(
-        <form className={styles.form} >
-            <InputDataII id="userName" type="text" placeholder="Nombre" name="Ingrese su nombre"/>
+        <Form className={styles.form} noValidate validated={validated} onSubmit={handleSubmit}>
+            <Row className="mb-3">
+            <InputDataII id="userName" 
+            type="text" 
+            placeholder="Nombre" 
+            name="Ingrese su nombre"
+            Feedback="Ingrese su nombre."
+            as={Col}
+            />
 
-            <InputDataII id="lastName" type="text" placeholder="Apellido" name="Ingrese su apellido"/>
+            <InputDataII id="lastName" 
+            type="text" 
+            placeholder="Apellido" 
+            name="Ingrese su apellido"
+            Feedback="Ingres su apellido"
+            as={Col}/>
+            </Row>
             
-            <InputDataII id="dpi" type="text" placeholder="DPI" name="Ingrese su DPI"/>
+            <InputDataII id="dpi" 
+            type="text" 
+            placeholder="DPI" 
+            name="Ingrese su DPI"
+            Feedback="Ingrese su DPI"/>
 
-            <InputDataII id="nomber_phone" type="text" placeholder="Numero de telefono." name="Ingrese su numero de Telefono" />
+            <InputDataII id="nomber_phone" 
+            type="text" 
+            placeholder="Numero de telefono." 
+            name="Ingrese su numero de Telefono" 
+            Feedback="Ingrese su numero de Telefono"/>
 
-            <InputDataII id="nomber_NIT" type="text" placeholder="NIT" name="Ingrese su numero de NIT"/>
+            <InputDataII id="nomber_NIT" 
+            type="text" 
+            placeholder="NIT" 
+            name="Ingrese su numero de NIT"
+            Feedback="Ingrese su numero NIT"/>
 
-            <InputDataII id="direction" type="text" placeholder="Direccion" name="Ingrese su numero de NIT"/>
+            <InputDataII id="direction" 
+            type="text" 
+            placeholder="Direccion" 
+            name="Ingrese su numero de NIT"
+            Feedback="Ingrese su NIT"/>
 
             <Buttons type="submit" content="Enviar" className="btn btn-primary"/>
         
-        </form>
+        </Form>
     )
 }
 
