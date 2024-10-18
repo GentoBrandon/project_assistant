01988c1401a751f3bff39574bbc3086f4f353a4c
Brandon Gento <brandonailon19@gmail.com>
2024-10-18 10:49:24 -0600

    Pagina de mostrar sub-Actividades

---
+++ Front-end/app/src/app/Tables/viewSubActivities/page.js
@@ -0,0 +1,11 @@
+import Title from "@/components/layouts/InputData/Title";
+import MainLayout from "@/components/template/principal/MainLayouts";
+
+function ViewSubActivitiesPage() {
+  return (
+    <MainLayout>
+        <Title title="Sub-Actividades" />
+    </MainLayout>
+  );
+}
+export default ViewSubActivitiesPage;
