559701a93609757591f86990979c9856a72af21d
kevinmartinez02 <kmartinezv7@miumg.edu.gt>
2024-10-24 13:02:04 -0600

    configuracion backend Reconocimiento de Cara

--- backend/api/api-employed/controllers/employedController.js
+++ backend/api/api-employed/controllers/employedController.js
@@ -1,5 +1,27 @@
 const employedModel = require('../models/employedModel');
+const {spawn} = require('child_process');
+const scriptFolder = 'C:\\Users\\KEVIN\\Documents\\Proyectos_Practica\\JavaScript\\Proyecto_analisis_Asistent\\project_assistant\\reconigtion_assistant\\FaceRecognition2';
+const runPythonProcess = (script, args = []) => {
+  return new Promise((resolve, reject) => {
+    const process = spawn('python', [`${scriptFolder}\\${script}`, ...args]);
 
+    process.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`);
+    });
+
+    process.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+    });
+
+    process.on('close', (code) => {
+      if (code === 0) {
+        resolve();
+      } else {
+        reject(new Error(`Process exited with code ${code}`));
+      }
+    });
+  });
+};
 const insertData = async (body) => {
   try {
     console.log('Ingresando');
@@ -10,7 +32,17 @@ const insertData = async (body) => {
       error.status = 400;
       throw error;
     }
+
     console.log('Empleado Ingresado Ingresado');
+    // Ejecutar el script de Python para capturar las fotos, pasando el ID del empleado
+    await runPythonProcess('capture.py', [result.id.id]);
+
+    console.log('Fotos capturadas correctamente.');
+
+    // Actualizar el modelo con las nuevas imágenes disponibles
+    await runPythonProcess('train_model.py', []);
+
+    console.log('Modelo actualizado correctamente.');
     return { status: 200, msg: 'Empleado Registrado con exito' };
   } catch (error) {
     throw error;
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,3 @@
 const App = require('./config/app');
-const app = new App(5000);
+const app = new App(6000);
 app.init();
